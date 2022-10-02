43fcb38a614b8c7dc19c3a465c41753ab55b54b9
mgebremariam7 <mgebremariam7@gatech.edu>
2022-10-01 21:47:04 -0400

    slight changes to BusinessRegisterView

--- Civility Optics/ViewModels/BusinessRegisterViewModel.swift
+++ Civility Optics/ViewModels/BusinessRegisterViewModel.swift
@@ -9,8 +9,8 @@ import SwiftUI
 
 class BusinessRegisterViewModel: ObservableObject {
   
-  func register(email: String, password: String, business_key: String, business_name: String, business_addr: String) {
-    NetworkingService.register(email: email, password: password, business_key: String, business_name: business_name, business_addr: business_addr) { result in
+  func bRegister(email: String, password: String, business_key: String, business_name: String, business_addr: String) {
+    NetworkingService.businessRegister(email: email, password: password, business_key: String, business_name: business_name, business_addr: business_addr) { result in
       AuthService.current.token = result?.token
       DispatchQueue.main.async {
         self.success = true
