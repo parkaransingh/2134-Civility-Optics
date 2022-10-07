f1813cecf373cf82f9968a9becfbd03565631aaf
Menelik <41522285+menelik-g@users.noreply.github.com>
2022-10-07 18:38:57 -0400

    Update CategoryMenuModel.swift

--- Civility Optics/ViewModels/CategoryMenuModel.swift
+++ Civility Optics/ViewModels/CategoryMenuModel.swift
@@ -9,16 +9,16 @@ import Foundation
 import SwiftUI
 
 class CategoryMenuModel: Hashable {
-  
+  //Creates the model for menu with hashable
   static func == (lhs: CategoryMenuModel, rhs: CategoryMenuModel) -> Bool {
     return lhs.title == rhs.title
   }
-  
+  //Creates the hasher
   func hash(into hasher: inout Hasher) {
     hasher.combine(title)
     hasher.combine(labels.map { $0.text })
   }
-  
+  //Title for the menu
   init(title: String, labels: [String]) {
     self.title = title
     self.labels = labels.map { ($0, false) }
@@ -26,7 +26,7 @@ class CategoryMenuModel: Hashable {
   
   let title: String
   var labels: [(text: String, isSelected: Bool)]
-  
+  //Index for the menu items
   func index(of label: String) -> Int {
     return labels.firstIndex { currentLabel in
       currentLabel.text == label
