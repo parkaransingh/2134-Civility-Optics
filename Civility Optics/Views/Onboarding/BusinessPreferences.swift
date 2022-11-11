af3effd3da164c983c01cfdfab0802de21a8edaf
Keith <keithtankj98@gmail.com>
2022-11-11 00:03:39 -0500

    update description

--- Civility Optics/Views/Onboarding/BusinessPreferences.swift
+++ Civility Optics/Views/Onboarding/BusinessPreferences.swift
@@ -8,32 +8,45 @@
 import SwiftUI
 
 struct BusinessPreferences: View {
-    var email: String
-    var bModel = BusinessProfileModel(email: email)
+    @ObservedObject var bModel: BusinessProfileModel = BusinessProfileModel(email: "")
+    //var email: String
+    //var bModel = BusinessProfileModel(email: email??, "")
     var rModel = RegisterViewModel()
-    var model = VenueDetailsModel(placeID: bModel.bpost.business.business_key, description: bModel.bpost.business.business_description)
+    //var model = VenueDetailsModel(placeID: bModel.bpost.business.business_key! ?? <#default value#>! ?? <#default value#>! ?? <#default value#>! ?? <#default value#>!!, description: bModel.bpost.business.business_description ?? <#default value#>)
     @State var newDescription = ""
 
     var body: some View {
       VStack(alignment: .leading, spacing: 4) {
         //VenueDetails(model: .init(placeID: bModel.bpost.business.business_key, description: bModel.bpost.business.business_description), email: self.email)
-        let s1 = "Business Name: " + bModel.bpost.business.business_name
-        let s2 = "Business Address: " + bModel.bpost.business.business_addr
-        let s3 = "Business Description: " + bModel.bpost.business.business_description
+          let s1 = "Business Name: " + (bModel.bpost.business.business_name ?? "")
+          let s2 = "Business Address: " + (bModel.bpost.business.business_addr ?? "")
+          let s3 = "Business Description: " + (bModel.bpost.business.business_description ?? "")
         Text(s1)
-        Spacer()
+        //Spacer()
         Text(s2)
-        Spacer()
+        //Spacer()
         Text(s3)
         Spacer()
-        TextField("Change your business page description here.",
-        text: $newDescription
-        )
-      .onSubmit {
-        rModel.businessUpdate(email: bModel.bpost.business.email, password: bModel.bpost.business.password, business_key: bModel.bpost.business.business_key, business_name: bModel.bpost.business.business_name, business_addr: bModel.bpost.business.business_addr, business_description: newDescription, token: self.description, AuthService.current.token ?? "")
-        //refresh page here
-        bModel.refreshModel()
-      }
+          if #available(iOS 15.0, *) {
+              TextField("Change your business page description here.",
+                        text: $newDescription
+              )
+              
+              Button {
+                  rModel.businessUpdate(email: bModel.bpost.business.email ?? "", business_key: bModel.bpost.business.business_key ?? "", business_name: bModel.bpost.business.business_name ?? "", business_addr: bModel.bpost.business.business_addr ?? "", business_description: newDescription ?? "", token: AuthService.current.token ?? "")
+                  //refresh page here
+                  bModel.refreshModel()
+              } label: {
+                  RoundedRectangle(cornerRadius: 20)
+                      .foregroundColor(.velvet)
+                      .overlay(Text("Update Description").foregroundColor(.white))
+                      .frame(height: 60)
+                  
+              }
+            
+          } else {
+              // Fallback on earlier versions
+          }
     }
   }
 }
