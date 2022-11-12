4aa7aa3e3e62710cc7658c794d6b4ac2239e283b
Keith <keithtankj98@gmail.com>
2022-11-12 09:54:45 -0500

    ui changes business_preferences

--- Civility Optics/Views/Onboarding/BusinessPreferences.swift
+++ Civility Optics/Views/Onboarding/BusinessPreferences.swift
@@ -21,33 +21,69 @@ struct BusinessPreferences: View {
           let s1 = "Business Name: " + (bModel.bpost.business.business_name ?? "")
           let s2 = "Business Address: " + (bModel.bpost.business.business_addr ?? "")
           let s3 = "Business Description: " + (bModel.bpost.business.business_description ?? "")
-        Text(s1)
-        //Spacer()
-        Text(s2)
-        //Spacer()
-        Text(s3)
-        Spacer()
-          if #available(iOS 15.0, *) {
-              TextField("Change your business page description here.",
-                        text: $newDescription
-              )
-              
-              Button {
-                  print(newDescription)
-                  
-                  rModel.businessUpdate(email: bModel.bpost.business.email ?? "", business_key: bModel.bpost.business.business_key ?? "", business_name: bModel.bpost.business.business_name ?? "", business_addr: bModel.bpost.business.business_addr ?? "", business_description: newDescription ?? "", token: AuthService.current.token ?? "")
-                  //refresh page here
-                  bModel.refreshModel()
-              } label: {
-                  RoundedRectangle(cornerRadius: 20)
-                      .foregroundColor(.velvet)
-                      .overlay(Text("Update Description").foregroundColor(.white))
-                      .frame(height: 60)
-                  
+          
+          VStack{
+              HStack{
+                  Text(s1)
+                      .font(.custom(
+                        "FontNameRound",
+                        fixedSize: 18))
+                      .fixedSize(horizontal: false, vertical: true)
+                      .multilineTextAlignment(.center)
+                      .padding()
+                      .frame(width: 400, height: 200)
+                      .background(Rectangle().fill(Color.red).shadow(radius: 6))
+                      .scaledToFit()
               }
-            
-          } else {
-              // Fallback on earlier versions
+              //Spacer()
+              HStack{
+                  Text(s2)
+                      .font(.custom(
+                        "FontNameRound",
+                        fixedSize: 18))
+                      .fixedSize(horizontal: false, vertical: true)
+                      .multilineTextAlignment(.center)
+                      .padding()
+                      .frame(width: 400, height: 200)
+                      .background(Rectangle().fill(Color.blue).shadow(radius: 6))
+                      .scaledToFit()
+              }
+              //Spacer()
+              HStack{
+                  Text(s3)
+                      .font(.custom(
+                        "FontNameRound",
+                        fixedSize: 18))
+                      .fixedSize(horizontal: false, vertical: true)
+                      .multilineTextAlignment(.center)
+                      .padding()
+                      .frame(width: 400, height: 200)
+                      .background(Rectangle().fill(Color.yellow).shadow(radius: 6))
+                      .scaledToFit()
+              }
+              Spacer()
+              if #available(iOS 15.0, *) {
+                  TextField("Change your business page description here:",
+                            text: $newDescription
+                  )
+                  
+                  Button {
+                      print(newDescription)
+                      
+                      rModel.businessUpdate(email: bModel.bpost.business.email ?? "", business_key: bModel.bpost.business.business_key ?? "", business_name: bModel.bpost.business.business_name ?? "", business_addr: bModel.bpost.business.business_addr ?? "", business_description: newDescription ?? "", token: AuthService.current.token ?? "")
+                      //refresh page here
+                      bModel.refreshModel()
+                  } label: {
+                      RoundedRectangle(cornerRadius: 20)
+                          .foregroundColor(.velvet)
+                          .overlay(Text("Update Description").foregroundColor(.white))
+                          .frame(height: 60)
+                      
+                  }
+                  
+              } else {
+                  // Fallback on earlier versions
+              }
           }
     }
   }
